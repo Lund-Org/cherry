42f3935df7e582c8c1867ec7de587b6e94badbc3
Florian Dewulf <florian.dewulf@epitech.eu>
2018-12-28 00:27:48 +0100

    Update a log and begin the json method

--- src/builtins/response.js
+++ src/builtins/response.js
@@ -2,59 +2,74 @@ const check = require('../helpers/check')
 const fs = require('../helpers/fs')
 
 function response (code, headers, content) {
+  headers['Content-Length'] = content.length
   this.writeHead(code, headers)
   this.end(content)
 }
 
-/**
- * A method to allow the download of files as response
- */
-async function download (str, options = null) {
-  let content = ''
-  let sourceFilename = null
-  let defaultOptions = {
+function defaultDownloadOptions () {
+  return Object.freeze({
     isFilename: true,
+    sourceFilename: null,
     downloadFilename: null,
     headers: {
       'Content-Disposition': 'attachment'
     },
     readFileOptions: 'utf8',
     statusCode: 200
-  }
+  })
+}
 
-  if (!options) {
-    sourceFilename = str
-    options = defaultOptions
-    options.downloadFilename = sourceFilename
-  } else {
-    options = Object.assign(defaultOptions, options)
-    if (options && check.isSelfDefinedAndValid(options.isFilename)) {
-      sourceFilename = str
-      if (!options.downloadFilename) {
-        options.downloadFilename = sourceFilename
-      }
-    } else {
-      content = str
+function defaultJSONOptions () {
+  return Object.freeze({
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    statusCode: 200
+  })
+}
+
+/**
+ * A method to allow the download of files as response
+ */
+async function download (str, refOptions = null) {
+  let content = ''
+  const options = Object.assign({}, defaultDownloadOptions(), { sourceFilename: str }, refOptions)
+
+  if (!refOptions) {
+    options.downloadFilename = str
+  } else if (check.isSelfDefinedAndValid(options.isFilename)) {
+    if (!options.downloadFilename) {
+      options.downloadFilename = options.sourceFilename
     }
   }
 
-  if (sourceFilename) {
-    content = await fs.readFile(sourceFilename, options.readFileOptions)
+  if (check.isSelfDefinedAndValid(options.isFilename)) {
+    content = await fs.readFile(options.sourceFilename, options.readFileOptions)
+  } else {
+    content = str
   }
 
   if (check.isDefinedAndNotNull(options, 'downloadFilename')) {
     options.headers['Content-Disposition'] += `; filename="${options.downloadFilename}"`
   }
-  options.headers['Content-Length'] = content.length
-  response.bind(this)(options.statusCode, options.headers, content)
+
+  response.call(this, options.statusCode, options.headers, content)
 }
 
 function html () {
   
 }
 
-function json () {
+function json (ref, refOptions = null) {
+  const options = Object.assign({}, defaultJsonOptions(), refOptions)
+  let content = ref
 
+  if (typeof ref !== 'string') {
+    content = JSON.stringify(ref)
+  }
+
+  response.call(this, options.statusCode, options.headers, content)
 }
 
 module.exports = {
